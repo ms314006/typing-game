5714a5fa4908b6d330993998bebdd6e8c8005a2b
GQSM <ms314006@hotmail.com>
2021-06-14 22:36:54 +0800

    add: 完成 typing game 的基本功能

---
+++ src/classes/TypingGame.js
@@ -0,0 +1,33 @@
+class TypingGame {
+  constructor() {
+    this.started = false;
+    this.currentLetter = null;
+  }
+
+  start() {
+    this.started = true;
+  }
+
+  stop() {
+    this.started = false;
+    this.currentLetter = null;
+  }
+
+  getTypingGameStarted() {
+    return this.started;
+  }
+
+  setNewCurrentLetter(letter) {
+    this.currentLetter = letter;
+  }
+
+  checkIsLetterCorrect(letter) {
+    return this.currentLetter === letter;
+  }
+
+  getCurrentLetter() {
+    return this.currentLetter;
+  }
+}
+
+export default TypingGame;
--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import TypingGame from '../../classes/TypingGame';
 
 const Body = styled.div`
   width: 100vw;
@@ -7,8 +9,105 @@ const Body = styled.div`
   font-family: 'Nanum Gothic', sans-serif;
 `;
 
-const LandingPage = () => (
-  <Body>hiii</Body>
-);
+const Main = (props) => {
+  const { typingGame } = props;
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [timerIntervalId, setTimerIntervalId] = useState(null);
+  const [started, setStarted] = useState(false);
+  const [currentLetter, setCurrentLetter] = useState(null);
 
-export default LandingPage;
+  const getRandomEnglishLetter = () => {
+    const ENGLISH_LETTER_COUNT = 26;
+    const newEnglishLetterAsciiCode = Math.floor(Math.random() * ENGLISH_LETTER_COUNT) + 1 + 64;
+    return String.fromCharCode(newEnglishLetterAsciiCode);
+  };
+
+  const startTypingGame = () => {
+    typingGame.start();
+    setStarted(typingGame.getTypingGameStarted());
+
+    typingGame.setNewCurrentLetter(getRandomEnglishLetter());
+    setCurrentLetter(typingGame.getCurrentLetter());
+
+    setSecondsRemaining(10);
+  };
+
+  const stopTypingGame = () => {
+    typingGame.stop();
+    setStarted(typingGame.getTypingGameStarted());
+    setCurrentLetter(typingGame.getCurrentLetter());
+  };
+
+  useEffect(() => {
+    if (secondsRemaining <= 0) {
+      stopTypingGame();
+    }
+  }, [secondsRemaining]);
+
+  useEffect(() => {
+    if (started) {
+      const ONE_SECOND = 1000;
+      const newTimerIntervalId = setInterval(() => {
+        setSecondsRemaining((currentSecondsRemaining) => currentSecondsRemaining - 1);
+      }, ONE_SECOND);
+      setTimerIntervalId(newTimerIntervalId);
+    } else {
+      clearInterval(timerIntervalId);
+    }
+  }, [started]);
+
+  useEffect(() => {
+    const handleTypingEvent = (e) => {
+      const keyDownLetter = e.key.toUpperCase();
+      const correct = typingGame.checkIsLetterCorrect(keyDownLetter);
+      if (correct) {
+        typingGame.setNewCurrentLetter(getRandomEnglishLetter());
+        setCurrentLetter(typingGame.getCurrentLetter());
+      }
+    };
+
+    if (started) {
+      window.addEventListener('keydown', handleTypingEvent);
+    } else {
+      window.removeEventListener('keydown', handleTypingEvent);
+    }
+  }, [started]);
+
+  return (
+    <Body>
+      <div>
+        { currentLetter }
+      </div>
+      <div>
+        {
+          started ? (
+            <button type="button" onClick={stopTypingGame}>
+              Stop
+            </button>
+          ) : (
+            <button type="button" onClick={startTypingGame}>
+              Start
+            </button>
+          )
+        }
+      </div>
+      <div>
+        { secondsRemaining }
+      </div>
+    </Body>
+  );
+};
+
+Main.propTypes = {
+  typingGame: PropTypes.shape({
+    start: PropTypes.func,
+    stop: PropTypes.func,
+    getTypingGameStarted: PropTypes.func,
+    setNewCurrentLetter: PropTypes.func,
+    getCurrentLetter: PropTypes.func,
+    checkIsLetterCorrect: PropTypes.func,
+  }).isRequired,
+};
+
+const typingGame = new TypingGame();
+export default () => <Main typingGame={typingGame} />;
